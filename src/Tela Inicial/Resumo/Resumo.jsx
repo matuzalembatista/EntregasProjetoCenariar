634fe0dc5156080acfea61f14fc0fd190ae9096b
matuzalembatista <matuzalem_pereira@hotmail.com>
2024-08-21 17:49:40 -0300

    Ajustes de CSS

--- src/Tela Inicial/Resumo/Resumo.jsx
+++ src/Tela Inicial/Resumo/Resumo.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
-import './Resumo.css'; // Vamos adicionar estilos em um arquivo CSS separado
+import React from "react";
+import "./Resumo.css"; // Vamos adicionar estilos em um arquivo CSS separado
 
 const Resumo = () => {
   return (
     <div className="container">
       <div className="left">
-      <br />
-      <br />
-      <br />
         <br />
-        <p className="text">Um espaço <br /> instagramável br para registrar fotos memoráveis</p>
+        <br />
+        <br />
+        <br />
+        <p className="left-text">
+          Um espaço instagramável para registrar fotos memoráveis
+        </p>
       </div>
       <div className="right">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor inLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt utcididunt ut labore etLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut dolore magna aliqua.</p>
+        <p className="right-texto">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor inLorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Sed do eiusmod tempor incididunt utcididunt ut labore etLorem
+          ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+          tempor incididunt ut dolore magna aliqua.
+        </p>
       </div>
     </div>
   );
